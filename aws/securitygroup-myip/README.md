d73200e399d874452cda10b9231d923dd5b45937
PastaMasta <pasta.masta2902@gmail.com>
2019-08-21 12:00:26 +0100

    Moved notes to README as I'll write this in other languages later

---
+++ aws/securitygroup-myip/README.md
@@ -0,0 +1,12 @@
+Security groups - myip
+===================================
+
+Replaces rules in a security group with my current public IP address
+Why? Non-static IP at home; easy updating from laptop on GSM / free WiFi
+
+TODO:
+-------
+* Test if IPv6 work
+* Check if rules already exist - only add if required
+* Handle rules from 0.0.0.0
+* Adding then removing seems a bit of a roundabout method to do this
--- aws/securitygroup-myip/securitygroup-myip.rb
+++ aws/securitygroup-myip/securitygroup-myip.rb
@@ -1,17 +1,5 @@
 #! /usr/bin/ruby
 
-#
-# Replaces rules in a security group with my current public IP address
-# Why? Non-static IP at home; easy updating from laptop on GSM / free WiFi
-#
-
-# TODO:
-# Test if IPv6 work
-# Check if rules already exist - only add if required
-# Handle rules from 0.0.0.0
-# Adding then removing seems a bit of a roundabout method to do this
-#
-
 require 'pry'
 require 'aws-sdk-ec2'
 require 'net/http'
